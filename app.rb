989aa59b5874dce07248f3ed07e7e3e9f0aecfcb
Margarita Potylitsyna <148562840+Ritumka@users.noreply.github.com>
2025-01-29 00:53:45 +0000

    staring on the app

--- app.rb
+++ app.rb
@@ -1,54 +1,14 @@
 require "sinatra"
 require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
 
 get("/") do
- erb(:homepage)
+  erb(:homepage)
 end
 
-get("/rock") do
-    moves = ["rock", "paper", "scissors"]
+get("/rock")
+  @user_move = ["rock", "paper", "scissors"].sample
 
-  @comp_move = moves.sample
-
-  if @comp_move == "rock"
-    @outcome = "tied"
-  elsif @comp_move == "paper"
-    @outcome = "lost"
-  else
-    @outcome = "won"
-  end
-
-erb(:rock)
-end
-
-get("/paper") do
-  moves = ["rock", "paper", "scissors"]
-
-  @comp_move = moves.sample
-
-  if @comp_move == "paper"
-    @outcome = "tied"
-  elsif @comp_move == "scissors"
-    @outcome = "lost"
-  else
-    @outcome = "won"
-  end
-
-  erb(:paper)
-end
-
-get("/scissors") do
-  moves = ["rock", "paper", "scissors"]
-
-  @comp_move = moves.sample
-
-  if @comp_move == "scissors"
-    @outcome = "tied"
-  elsif @comp_move == "rock"
-    @outcome = "lost"
-  else
-    @outcome = "won"
-  end
-
-  erb(:scissors)
+  erb(:rock)
 end
